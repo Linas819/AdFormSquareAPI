1518844cb83905e7b627083d4c62f6c19550d820
Linas819 <63401219+Linas819@users.noreply.github.com>
2024-06-11 16:19:58 +0300

    SquareController.GetSquares method done

--- AdFormTask/Controllers/SquaresController.cs
+++ AdFormTask/Controllers/SquaresController.cs
@@ -16,7 +16,7 @@ namespace AdFormTask.Controllers
             this.squareService = squareService;
         }
         // !!!!!TEST COORDINATES!!!!!
-        // [(-1;1), (1;1), (1;-1), (-1;-1)]
+        // [(-1;1), (1;1), (1;-1), (-1;-1), (0;0), (2;2), (0;2), (2;0), (3;0)]
         [HttpGet]
         public IActionResult GetSquares(string pointsString)
         {
@@ -24,6 +24,57 @@ namespace AdFormTask.Controllers
             pointsString = squareService.FormatPointString(pointsString);
             string[] coordinates = pointsString.Split(", ");
             List<Point> points = squareService.GetPoints(coordinates);
+            foreach (Point point1 in points)
+            {
+                foreach (Point point2 in points)
+                {
+                    if (Object.Equals(point1, point2))
+                        continue;
+                    else
+                    {
+                        foreach (Point point3 in points)
+                        {
+                            if (Object.Equals(point1, point3) || Object.Equals(point2, point3))
+                                continue;
+                            else
+                            {
+                                foreach (Point point4 in points)
+                                {
+                                    if (Object.Equals(point1, point4) || Object.Equals(point2, point4) || Object.Equals(point3, point4))
+                                        continue;
+                                    else
+                                    {
+                                        bool coordinatesAdded = false;
+                                        if (squareCoordinates.Count == 0)
+                                        {
+                                            if (squareService.IsSquare(point1, point2, point3, point4))
+                                                squareCoordinates.Add("[(" + point1.x.ToString() + ";" + point1.y.ToString() + "), ("
+                                                    + point2.x.ToString() + ";" + point2.y.ToString() + "), (" + point3.x.ToString() + ";" + point3.y.ToString() + "), ("
+                                                    + point4.x.ToString() + ";" + point4.y.ToString() + ")]");
+                                            break;
+                                        }
+                                        foreach (string squareCoordinate in squareCoordinates)
+                                        {
+                                            if (squareCoordinate.IndexOf(point1.x.ToString() + ";" + point1.y.ToString()) > 0
+                                                && squareCoordinate.IndexOf(point2.x.ToString() + ";" + point2.y.ToString()) > 0
+                                                && squareCoordinate.IndexOf(point3.x.ToString() + ";" + point3.y.ToString()) > 0
+                                                && squareCoordinate.IndexOf(point4.x.ToString() + ";" + point4.y.ToString()) > 0)
+                                            {
+                                                coordinatesAdded = true;
+                                                break;
+                                            }
+                                        }
+                                        if(coordinatesAdded == false && squareService.IsSquare(point1, point2, point3, point4))
+                                            squareCoordinates.Add("[(" + point1.x.ToString() + ";" + point1.y.ToString() + "), ("
+                                                + point2.x.ToString() + ";" + point2.y.ToString() + "), (" + point3.x.ToString() + ";" + point3.y.ToString() + "), ("
+                                                + point4.x.ToString() + ";" + point4.y.ToString() + ")]");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             return Ok(new
             {
                 squaresCoordinates = squareCoordinates,
--- AdFormTask/obj/Debug/net8.0/AdFormTask.AssemblyInfo.cs
+++ AdFormTask/obj/Debug/net8.0/AdFormTask.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdFormTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6955457c5ce32959006a2b12495126695da49f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2752ac1e140bb62a06b311e5d91bf74e489d61cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdFormTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdFormTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
